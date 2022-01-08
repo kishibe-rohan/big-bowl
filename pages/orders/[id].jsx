3b596a0ee24993c40c4fdd49611688a955724bfb
kishibe-rohan <devrohan2000@gmail.com>
2022-01-08 20:26:00 +0530

    added order tracking page

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,103 @@
+import Image from 'next/image'
+import styles from '../../styles/Orders.module.css'
+
+const Order = () => {
+  const status = 1;
+
+  const statusClass = (index) => {
+      if(index - status < 1)
+      return styles.done;
+      if(index - status === 1)
+      return styles.inProgress;
+      if(index - status > 1)
+      return styles.undone
+  }
+
+   return(
+     <div className={styles.container}>
+        <div className={styles.left}>
+            <div className={styles.row}>
+                <table className={styles.table}>
+                    <tr className={styles.trTitle}>
+                        <th>Order ID</th>
+                        <th>Customer</th>
+                        <th>Address</th>
+                        <th>Total</th>
+                    </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <span className={styles.id}>123456</span>
+                        </td>
+                        <td>
+                            <span className={styles.name}>John Doe</span>
+                        </td>
+                        <td>
+                            <span className={styles.address}>Laxmi Road,Pune-411011</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>$300</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div className={styles.row}>
+                <div className={statusClass(0)}>
+                    <Image src="/images/paid.png"  width={30} height={30} alt=""/>
+                    <span>Paid</span>
+                    <div className={styles.checkedIcon}>
+                        <Image src="/images/check.png" className={styles.checkedIcon} width={20}
+                height={20}
+                alt=""/>
+                    </div>
+                </div>
+            <div className={statusClass(1)}>
+                    <Image src="/images/cook.png"  width={30} height={30} alt=""/>
+                    <span>Preparing</span>
+                    <div className={styles.checkedIcon}>
+                        <Image src="/images/check.png" className={styles.checkedIcon} width={20}
+                height={20}
+                alt=""/>
+                    </div>
+            </div> 
+                <div className={statusClass(2)}>
+                    <Image src="/images/delivery.png"  width={40} height={30} alt=""/>
+                    <span>On the way</span>
+                    <div className={styles.checkedIcon}>
+                        <Image src="/images/check.png" className={styles.checkedIcon} width={20}
+                height={20}
+                alt=""/>
+                    </div>
+                </div>
+                <div className={statusClass(3)}>
+                    <Image src="/images/delivered.png"  width={30} height={30} alt=""/>
+                    <span>Delivered</span>
+                    <div className={styles.checkedIcon}>
+                        <Image src="/images/check.png" className={styles.checkedIcon} width={20}
+                height={20}
+                alt=""/>
+                    </div>
+                </div>
+        </div>
+     </div>
+     <div className={styles.right}>
+         <div className={styles.wrapper}>
+             <h2 className={styles.title}>CART TOTAL</h2>
+             <div className={styles.totalText}>
+                 <b className={styles.totalTextTitle}>Subtotal:</b>$20.00
+             </div>
+             <div className={styles.totalText}>
+                 <b className={styles.totalTextTitle}>Discount:</b>$0.00
+             </div>
+             <div className={styles.totalText}>
+                 <b className={styles.totalTextTitle}>Total:</b>$20.00
+             </div>
+             <button disabled  className={styles.button}>
+                 PAID
+             </button>
+         </div>
+     </div>
+     </div>
+   )
+}
+
+export default Order;
