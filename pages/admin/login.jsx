195044c775565322a9545c58f59a94df24826c3f
kishibe-rohan <67476046+kishibe-rohan@users.noreply.github.com>
2022-03-03 20:24:20 +0530

    Update login.jsx

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,152 +1,68 @@
-import Image from 'next/image'
-import {useState} from 'react'
-import axios from 'axios'
-import styles from '../../styles/Admin.module.css'
+import { useState } from "react";
+import {useRouter} from 'next/router'
 
-const Index = ({orders,products}) => {
+import axios from 'axios';
+import styles from '../../styles/Login.module.css'
 
-    const [productList,setProductList] = useState(products);
-    const [orderList,setOrderList] = useState(orders);
+const Login = () => {
+    const [username,setUsername] = useState(null);
+    const [password,setPassword] = useState(null);
+    const [error, setError] = useState(false);
+    const router = useRouter();
 
-    const status = ["Preparing", "On The Way", "Delivered"];
-
-    const handleDelete = async(id) => {
+    const handleSubmit = async() => {
         try{
-            const res = await axios.delete("https://big-bowl.vercel.app/api/products/" + id);
-            setProductList(productList.filter((product) => product._id !== id ))
-        }catch(err)
-        {
-            console.log(err)
-        }
-    }
 
-    const handleStatus = async(id) => {
-        const item = orderList.filter((order) => order._id === id)[0];
-        const currentStatus = item.status;
-
-        try{
-            const res = await axios.put("https://big-bowl.vercel.app/api/orders/" + id,{
-                status:currentStatus+1
+            
+            await axios.post("https://big-bowl.vercel.app/api/login",{
+                username,
+                password
             })
 
-            setOrderList([
-                res.data,...orderList.filter((order) => order._id !== id)
-            ])
+            router.push("/admin");
         }catch(err)
         {
-            console.log(err)
+            setError(true);
+           
         }
     }
-
 
     return(
         <div className={styles.container}>
-          <div className={styles.item}>
-              <h1 className={styles.title}>Products</h1>
-              <table className={styles.table}>
-                  <tbody>
-                  <tr className={styles.trTitle}>
-                      <th>Image</th>
-                      <th>Id</th>
-                      <th>Title</th>
-                      <th>Price</th>
-                      <th>Action</th>
-                  </tr>
-                  </tbody>
-                  {
-                      productList.map((product) => (
-                          <tbody key={product._id}>
-                              <tr className={styles.trTitle}>
-                              <td>
-                  <Image
-                    src={product.img}
-                    width={50}
-                    height={50}
-                    objectFit="cover"
-                    alt=""
-                  />
-                </td>
-                <td>{product._id.slice(0, 5)}...</td>
-                <td>{product.title}</td>
-                <td>${product.prices[0]}</td>
-                <td>
-                    <button className={styles.button}>Edit</button>
-                    <button className={styles.button} onClick={() => handleDelete(product._id)}>
-                        Delete
-                    </button>
-                </td>
-                              </tr>
-                          </tbody>
-                      ))
-                  }
-              </table>
-          </div>
-          <div className={styles.item}>
-              <h1 className={styles.title}>Orders</h1>
-              <table className={styles.table}>
-                  <tbody>
-                      <tr className={styles.trTitle}>
-                        <th>Id</th>
-                        <th>Customer</th>
-                        <th>Total</th>
-                        <th>Payment</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                      </tr>
-                  </tbody>
-                  {
-                      orderList.map((order) => (
-                          <tbody key={order._id}>
-                              <tr className={styles.trTitle}>
-                                  <td>{order._id.slice(0,5)}...</td>
-                                  <td>{order.customer}</td>
-                                  <td>${order.total}</td>
-                                  <td>
-                                      {order.method === 0? <span>COD</span>:<span>PAID</span>}
-                                  </td>
-                                  <td>{status[order.status]}</td>
-                                  <td>
-                                      <button onClick={() => handleStatus(order._id)}>
-                                          Next Stage
-                                      </button>
-                                  </td>
-                              </tr>
-                          </tbody>
-                      ))
-                  }
-              </table>
-          </div>
+            <div className={styles.wrapper}>
+                <h1 className={styles.text}>Admin Login</h1>
+                <input className={styles.input} placeholder = "Username" onChange={(e) => setUsername(e.target.value)}/>
+                <input className={styles.input} placeholder = "Password" onChange={(e) => setPassword(e.target.value)}/>
+                <button className={styles.button} onClick={handleSubmit}>SIGN IN</button>
+                {
+                    error && <span className={styles.error}>Invalid Credentials</span>
+                }
+            </div>
         </div>
     )
 }
-
 
 export const getServerSideProps = async(ctx) => {
     const myCookie = ctx.req?.cookies || "";
 
     
-    if(myCookie.bbtoken !== process.env.TOKEN)
+    if(myCookie.bbtoken === process.env.TOKEN)
     {
         return{
             redirect:{
-                destination:"/admin/login",
+                destination:"/admin",
                 permanent:false
             }
         }
     }
-    
-    
-
-    const productRes = await axios.get("https://big-bowl.vercel.app/api/products");
-    const orderRes = await axios.get("https://big-bowl.vercel.app/api/orders");
 
     return{
         props:{
-            orders:orderRes.data,
-            products:productRes.data
+           msg:"Admin Logged In"
         }
     }
     
 
 }
-export default Index;
+
+export default Login;
