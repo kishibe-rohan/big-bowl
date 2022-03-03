5854e167e7fbe1ec1647313b362fe19f3703a31f
kishibe-rohan <67476046+kishibe-rohan@users.noreply.github.com>
2022-03-03 20:35:04 +0530

    Update [id].jsx

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -103,7 +103,7 @@ const Order = ({order}) => {
 }
 
 export const getServerSideProps = async({params}) => {
-    const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+    const res = await axios.get(`https://big-bowl.vercel.app/api/orders/${params.id}`);
     return{
         props:{
             order:res.data
